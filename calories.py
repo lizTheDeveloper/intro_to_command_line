ff77e759eeaffe051973c87916a8617be00b8630
lizthedeveloper <915684+lizTheDeveloper@users.noreply.github.com>
2024-05-27 21:02:15 +0000

    calculate the weekly calorie budget

---
+++ calories.py
@@ -0,0 +1,10 @@
+base_daily_calories = 2000
+days = 7
+
+calories = base_daily_calories * days
+
+display_message = "You have a budget of " + str(calories) + " calories this week"
+
+print(display_message)
+
+print("have a good " + "day")
